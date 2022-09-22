691d16178bb5333ea22dcb28bf0f8e30c3b7fbb4
gopherine <atharvajava@gmail.com>
2022-09-22 16:31:41 +0530

    feat(dev): add cli app skeleton
    create a brainmap/ skeleton to create cli app for the app , add bool flags functionality with a thirdparty lib

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
@@ -24,4 +25,48 @@ func main() {
 	}
 
 	_ = client
+
+	// Creating a CLI app with flags and actions.
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "deploy",
+				Aliases: []string{"d"},
+				Usage:   "Deploy smart contract",
+			},
+			&cli.BoolFlag{
+				Name:    "check",
+				Aliases: []string{"c"},
+				Usage:   "Check if smart contract is deployed",
+			},
+			&cli.BoolFlag{
+				Name:    "read",
+				Aliases: []string{"r"},
+				Usage:   "Read smart contract info",
+			},
+			&cli.BoolFlag{
+				Name:    "transact",
+				Aliases: []string{"t"},
+				Usage:   "Read smart contract info",
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			if cCtx.Bool("deploy") {
+				log.Print("will contain logic for deploying")
+			} else if cCtx.Bool("check") {
+				log.Print("Will call and check reciept to confirm deployment")
+			} else if cCtx.Bool("read") {
+				log.Print("Will read from contract some basic functions to check balance and symbol")
+			} else if cCtx.Bool("transact") {
+				log.Print("Will transfer tokens from one address to other")
+			}
+
+			return nil
+		},
+	}
+
+	// Running the app with the arguments passed in the command line.
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal().Err(err)
+	}
 }
